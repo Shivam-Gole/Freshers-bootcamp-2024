94e2cc5c3274fd797c53c0f584a82dd46c746ab3
avibhai45 <143583412+avibhai45@users.noreply.github.com>
2024-01-09 14:40:26 +0530

    Update Searchfunction.cpp

--- Searchfunction.cpp
+++ Searchfunction.cpp
@@ -5,13 +5,18 @@ vector<int> filter(vector<int> &arr , function<bool(int)> criteria) {
     vector<int> answer;
     
     for(auto cont &element : arr){
-        if(criteria(element))
+        if(criteria(element)) {
         answer.push_back(element);
+        }    
     }
     return element;
 }
 
 int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto is_even =  { return x % 2 == 0; };
+    auto is_odd =  { return x % 2 != 0; };
+    vector<function<bool(int)>> criteria_list = {is_even, is_odd};
     
     cout << "Hello !";
     return 0;
