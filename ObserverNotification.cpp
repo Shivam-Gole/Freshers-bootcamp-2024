712cf65eb9028fadc8262a32dbef472f26c96379
Shivam-Gole <143583412+Shivam-Gole@users.noreply.github.com>
2024-01-15 11:23:49 +0530

    Create ObserverNotification.cpp

---
+++ ObserverNotification.cpp
@@ -0,0 +1,70 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class IObserver(){
+public:
+  virtual ~IObserver(){};
+  virtual void Update(const string &message_from_subject) = 0;
+};
+
+class ConcreteObserver : public IObserver{
+    public:
+    void update(string state){
+        
+    }
+};
+
+class Thread {
+    private: 
+    int id;
+    string state;
+    string priority;
+    string culture;
+    
+    void notify(){
+        
+    }
+    
+    public:
+    void Thread(){
+        state = "start";
+        this->notify();
+    }
+    void start(){
+        this->state = "running";
+        this->notify();
+    }
+    void abort(){
+        this->state = "aborted";
+        this->notify();
+    }
+    void sleep(){
+        this->state = "sleep";
+        this->notify();
+    }
+    void wait(){
+        this->state = "waiting";
+        this->notify();
+    }
+    void suspended(){
+        this->state = "suspended";
+        this->notify();
+    }
+    
+    void subscribe(){
+        
+    }
+    
+    void unsubsribe(){
+        
+    }
+};
+
+int main() {
+    Thread obj;
+   
+
+    return 0;
+}
