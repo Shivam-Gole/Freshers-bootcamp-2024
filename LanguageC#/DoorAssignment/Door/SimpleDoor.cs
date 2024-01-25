94a0637b3ec3f7152dec8c33308f25374a3568a9
Shivam-Gole <143583412+Shivam-Gole@users.noreply.github.com>
2024-01-25 09:37:38 +0530

    Update SimpleDoor.cs

--- LanguageC#/DoorAssignment/Door/SimpleDoor.cs
+++ LanguageC#/DoorAssignment/Door/SimpleDoor.cs
@@ -1,1 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorEvent
+{
+    
+    public delegate void DoorEventHandler(object sender, EventArgs e);
+
+    public class SimpleDoor : IDoor
+    {
+        public event DoorEventHandler DoorOpened;
+        public event DoorEventHandler DoorClosed;
+
+        private bool isOpen;
+
+        public bool IsOpen
+        {
+            get { return isOpen; }
+            protected set { isOpen = value; }
+        }
+
+        public virtual void Open()
+        {
+            IsOpen = true;
+            OnDoorOpened();
+        }
+
+        public void Close()
+        {
+            IsOpen = false;
+            OnDoorClosed();
+        }
+
+        protected virtual void OnDoorOpened()
+        {
+            DoorOpened?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnDoorClosed()
+        {
+            DoorClosed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
 
