831ffe555bcac066a904a121ae0fa6b3292aefd2
Shivam-Gole <143583412+Shivam-Gole@users.noreply.github.com>
2024-01-25 09:40:24 +0530

    Create Notify.cs

---
+++ LanguageC#/DoorAssignment/Door/Notify.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorEvent
+{
+    public class Notify
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("Pager notification sent!");
+        }
+    }
+}
